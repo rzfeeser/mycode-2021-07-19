6e8f475e9558b051932b20a7a96a31d25643f94b
RZFeeser <rzfeeser@users.noreply.github.com>
2021-07-20 20:54:33 +0000

    end of day 2

---
+++ example12-farms.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lab 24 - challenge 01"""
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+
+for farm in farms:
+    print(farm.get("name"), end=": ")
+    
+    for animal in farm.get("agriculture"):
+        if animal == farm.get("agriculture")[-1]:
+            print(animal)
+        else:
+            print(animal, end=", ")
